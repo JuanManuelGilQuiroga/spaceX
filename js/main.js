8410fcd3d9e7bb85313cfbc00e3bea59781af33e
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-05-22 11:51:06 -0500

    feat: :sparkles: Basic configuration for the capsules

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
 import { getAllRocketEngineTotal } from "./modules/rockets.js";
 import { 
+    load,
+    paginationCapsules,
     paginationRockets
 } from "./modulesComponents/pagination.js";
 
 
-let pagination = document.querySelector("#footer__aside__right");
-pagination.innerHTML = "";
-pagination.append(await paginationRockets())
+let footerSelect = async(e, id)=>{
+    e.preventDefault();
+    await load();
+    let li = document.querySelectorAll(".footer ul li")
+    for(let val of li){
+        let [a] = val.children
+        a.classList.remove('select');
+    }
+    let [a] = id.children
+    a.classList.add('select');   
+}
+
+let rocket = document.querySelector("#rocket")
+rocket.addEventListener("click", async(e)=>{
+    await footerSelect(e, rocket)
+    let paginacion = document.querySelector("#paginacion");
+    paginacion.innerHTML = ""
+    paginacion.append(await paginationRockets())
+} )
+
+let capsules = document.querySelector("#capsules")
+capsules.addEventListener("click", async(e)=>{
+    await footerSelect(e, capsules)
+    let paginacion = document.querySelector("#paginacion");
+    paginacion.innerHTML = ""
+    paginacion.append(await paginationCapsules())
+})
+
+
+capsules.click();
--- js/modulesComponents/pagination.js
+++ js/modulesComponents/pagination.js
@@ -31,6 +31,81 @@ import {
     progressSecondStageHeightRocket,
 } from "./progressBar.js";
 
+import { 
+    getAllCapsules 
+} from "../modules/capsules.js";
+
+
+export const load = async()=>{
+    let header__title = document.querySelector("#header__title");
+    header__title.innerHTML = `
+        <div class="load"></div>
+    `;
+
+    let main__aside__left = document.querySelector("#main__aside__left");
+    main__aside__left.innerHTML = `
+        <div class="load"></div>
+        <div class="load"></div>
+        <div class="load"></div>
+        <div class="load"></div>
+        <div class="load"></div>
+    `;
+
+    let section__information__1 = document.querySelector("#section__information__1")
+    section__information__1.innerHTML = `
+        <div class="load" style="height: 150px;"></div>
+    `;
+
+    let table__1 = document.querySelector("#table__1")
+    table__1.innerHTML = `
+        <div class="load" style="height: 160px;"></div>
+    `;
+
+    let section__image = document.querySelector("#section__image")
+    section__image.innerHTML = `
+        <div class="load" style="height: 350px"></div>
+    `;
+
+
+    let table__2 = document.querySelector("#table__2")
+    table__2.innerHTML = `
+        <div class="load" style="height: 160px;"></div>
+    `;
+
+    let main__aside__right = document.querySelector("#main__aside__right")
+    main__aside__right.innerHTML = `
+        <div class="load"></div>
+        <div class="load"></div>
+        <div class="load"></div>
+        <div class="load"></div>
+        <div class="load"></div>
+    `;
+}
+export const clear = async()=>{
+    let header__title = document.querySelector("#header__title");
+    header__title.innerHTML = ``;
+
+    let main__aside__left = document.querySelector("#main__aside__left");
+    main__aside__left.innerHTML = ``;
+
+    let section__information__1 = document.querySelector("#section__information__1")
+    section__information__1.innerHTML = ``;
+
+    let table__1 = document.querySelector("#table__1")
+    table__1.innerHTML = ``;
+
+    let section__image = document.querySelector("#section__image")
+    section__image.innerHTML = ``;
+
+
+    let table__2 = document.querySelector("#table__2")
+    table__2.innerHTML = ``;
+
+    let main__aside__right = document.querySelector("#main__aside__right")
+    main__aside__right.innerHTML = ``;
+
+}
+
 
 const getRocketsId = async(e)=>{
     e.preventDefault();
@@ -83,6 +158,70 @@ export const paginationRockets = async()=>{
         div.appendChild(a);
     });
     let [a1,a2,a3,a4] = div.children
+    a1.click();
+    // <div class="buttom__paginacion">
+    //     <a href="#">&laquo;</a> 
+    //     <a href="#" class="activo">1</a>
+    //     <a href="#">2</a>
+    //     <a href="#">3</a>
+    //     <a href="#">4</a>
+    //     <a href="#">&raquo;</a>
+    // </div>
+    return div;
+}
+
+const getCapsulesId = async(e)=>{
+    e.preventDefault();
+    if(e.target.dataset.page){
+        let paginacion = document.querySelector("#paginacion");
+        paginacion.innerHTML = ""
+        paginacion.append(await paginationCapsules(Number(e.target.dataset.page)))
+    }
+    let a = e.target.parentElement.children;
+    for(let val of a){
+        val.classList.remove('activo');
+    }
+    e.target.classList.add('activo');
+    
+
+    // let Rocket = await getAllRocketsId(e.target.id);
+    // console.log(Rocket);
+
+    // await informationRockets(Rocket.country, Rocket.description)
+    
+}
+
+export const paginationCapsules = async(page=1, limit=4)=>{  
+     
+    let {docs, pagingCounter, totalPages, nextPage} = await getAllCapsules(page, limit)
+
+    let div = document.createElement("div");
+    div.classList.add("buttom__paginacion")
+
+    
+    let start = document.createElement("a");
+    start.setAttribute("href","#");
+    start.innerHTML = "&laquo";
+    start.setAttribute("data-page", (page==1) ? totalPages : page-1)
+    start.addEventListener("click", getCapsulesId)
+    div.appendChild(start);
+    docs.forEach((val,id) => {
+        let a = document.createElement("a");
+        a.setAttribute("href","#");
+        a.id = val.id;
+        a.textContent = pagingCounter;
+        a.addEventListener("click", getCapsulesId)
+        div.appendChild(a);
+        pagingCounter++
+    });
+    let end = document.createElement("a");
+    end.setAttribute("href","#");
+    end.innerHTML = "&raquo;";
+    end.setAttribute("data-page", (page && nextPage) ? page+1 : 1)
+    end.addEventListener("click", getCapsulesId)
+    div.appendChild(end);
+    console.log(div);
+    let [back, a1,a2,a3,a4, next] = div.children
     a1.click();
     // <div class="buttom__paginacion">
     //     <a href="#">&laquo;</a> 
